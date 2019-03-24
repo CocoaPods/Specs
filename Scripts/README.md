14d82643f9ded504ab7205040ac4e7b3e672c99d
James Treanor <jtreanor3@gmail.com>
2019-03-24 21:01:24 +0000

    Combine merge and adding indexes into one commit

--- Scripts/README.md
+++ Scripts/README.md
@@ -4,4 +4,4 @@ The two scripts are a part of updating CocoaPods to work with a CDN instead of o
 `git` repos.
 
 - [`add_indexes.sh`](add_indexes.sh) - Adds an index for every Pod, this should probably only need to be used once.
-- [`add_incremental_index.sh`](add_incremental_index.sh) - Uses git to update only Pods which have changed in the given commit.
+- [`add_incremental_index.sh`](add_incremental_index.sh) - Uses git to update only Pods which have changed in the given commit range.
--- Scripts/add_incremental_index.sh
+++ Scripts/add_incremental_index.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
 set -eo pipefail
 
-COMMIT="$1"
+FIRST_COMMIT="$1"
+SECOND_COMMIT="$2"
 
-# Get the changed files from the given commit
+# Get the changed files from the given commits
 
-CHANGED_FILES=$(git diff --name-only $COMMIT~ $COMMIT Specs)
+CHANGED_FILES=$(git diff --name-only $FIRST_COMMIT...$SECOND_COMMIT Specs)
 
 # Cut paths down to `Specs/9/f/e/MessagingSDK/1.3.9`
 # Insert `1.3.9` in sorted order into `Specs/9/f/e/MessagingSDK/index.txt` if not already present
 
 echo "$CHANGED_FILES" | cut -d / -f 1,2,3,4,5,6 | xargs -I {} \
     bash -c 'POD_INDEX="$(dirname "{}")/index.txt" ; touch $POD_INDEX ; \
-             basename "{}" | sort -o "$POD_INDEX" -m -u - "$POD_INDEX"'
+             basename "{}" | sort -o "$POD_INDEX" -m -u - "$POD_INDEX" && echo "$POD_INDEX"'
 
-# Cut paths down to `Specs/9/f/e/MessagingSDK`
+# Cut paths down to `MessagingSDK`
 # Insert `MessagingSDK` in sorted order into all_pods.txt if not already present
 
 echo "$CHANGED_FILES" | cut -d / -f 5 | xargs -I {} \
     bash -c 'echo "{}" | sort -o all_pods.txt -m -u - all_pods.txt'
+echo "all_pods.txt"
