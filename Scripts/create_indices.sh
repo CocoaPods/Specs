de071a0528181f489401598a8c5bfc6bc58160a8
Igor Makarov <igormaka@gmail.com>
2019-05-22 10:07:00 +0300

    Netlify massaging

--- Scripts/create_indices.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -x
-set -eo pipefail
-
-mkdir _site || true
-
-ruby Scripts/create_pods_and_versions_index.rb > _site/all_pods_versions.txt
-gzip < _site/all_pods_versions.txt > _site/all_pods_versions.gz
-cp Scripts/netlify_redirects.txt _site/_redirects
