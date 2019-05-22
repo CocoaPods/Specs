ffef074abf58b06e5ab4a95c3714b73fde3b4a17
Igor Makarov <igormaka@gmail.com>
2019-05-22 10:46:28 +0300

    Fix script

--- Scripts/netlify_deploy.sh
+++ Scripts/netlify_deploy.sh
@@ -4,5 +4,5 @@ set -eo pipefail
 
 mkdir _site || true
 
-ruby Scripts/create_pods_and_versions_index.rb | gzip > _site/all_pods_versions.txt
+ruby Scripts/create_pods_and_versions_index.rb | gzip > _site/all_pods_versions.gz
 cp Scripts/netlify_redirects.txt _site/_redirects
