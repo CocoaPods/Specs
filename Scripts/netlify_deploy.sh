62289d821a5e0100ca127e4d918a3379f41f79ee
Igor Makarov <igormaka@gmail.com>
2019-05-30 12:27:39 +0300

    Also generate tarball of all the shard indexes

--- Scripts/netlify_deploy.sh
+++ Scripts/netlify_deploy.sh
@@ -3,12 +3,19 @@ set -eo pipefail
 
 mkdir _site || true
 
-find Specs -mindepth 3 -maxdepth 3 | (
+find Specs -mindepth 3 -maxdepth 3 | {
   while read dirname; do
     without_specs="${dirname/Specs\//}"
     slashes_to_underscores="${without_specs//\//_}"
-    ruby Scripts/create_pods_and_versions_index.rb "$dirname" > "_site/all_pods_versions_$slashes_to_underscores.txt"
+    ruby Scripts/create_pods_and_versions_index.rb "$dirname" > "_site/all_pods_versions_$slashes_to_underscores.txt" &
   done
+}
+
+wait
+
+(
+  cd _site 
+  tar -zcf all_pods_versions_tarball.tar.gz all_pods_versions_*.txt
 )
 
 ruby Scripts/create_pods_and_versions_index.rb | gzip > _site/all_pods_versions.gz
