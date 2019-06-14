f92bfd6948dfb37471fa4bf25598a633bc5f1cb0
Igor Makarov <igormaka@gmail.com>
2019-06-14 13:52:20 +0300

    Redirect root to blog post

--- Scripts/netlify_deploy.sh
+++ Scripts/netlify_deploy.sh
@@ -3,6 +3,8 @@ set -eo pipefail
 
 mkdir _site || true
 
+# generate sharded index
 ruby Scripts/create_pods_and_versions_index.rb _site
 
+# add redirects to GitHub
 cp Scripts/netlify_redirects.txt _site/_redirects
