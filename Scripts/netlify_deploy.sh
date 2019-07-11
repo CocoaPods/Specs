8793cf0adb97138a64459bf87cf74923bda42294
Igor Makarov <igormaka@gmail.com>
2019-07-11 11:16:05 +0300

    rewrite index generator and add deprecated podspec index

--- Scripts/netlify_deploy.sh
+++ Scripts/netlify_deploy.sh
@@ -4,7 +4,7 @@ set -eo pipefail
 mkdir _site || true
 
 # generate sharded index
-ruby Scripts/create_pods_and_versions_index.rb _site
+bundle exec ruby Scripts/create_pods_and_versions_index.rb _site
 
 # add redirects to GitHub
 cp Scripts/netlify_redirects.txt _site/_redirects
